75f834090a5c8e7db063958b8ef552e3503c5faa
gcosta815 <gcosta815@gmail.com>
2024-03-05 22:48:18 -0300

    2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchName, setSearchName] = useState('');
+  const [message, setMessage] = useState({msg: null, type: null});
 
   useEffect(() => {
     personService
@@ -19,6 +21,13 @@ const App = () => {
   const handleNewName = (event) => setNewName(event.target.value);
   const handleNewNumber = (event) => setNewNumber(event.target.value);
   const handleSearchName = (event) => setSearchName(event.target.value);
+
+  const showMessage = (msg, type) => {
+    setMessage({msg: msg, type: type})
+      setTimeout(() => {
+        setMessage({msg: null, type: null})
+      }, 2000);
+  }
 
   const addName = (event) => {
     event.preventDefault();
@@ -32,17 +41,23 @@ const App = () => {
       const newPerson = { name: newName, number: newNumber, id: (maxId + 1).toString() };
       personService
         .create(newPerson)
-        .then(person => setPersons(persons.concat(person)))
+        .then(person => 
+          { setPersons(persons.concat(person));
+            showMessage(`Added ${person.name}`, 'success');
+          })
     }
     else {
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
         personService
           .update(findPerson.id, {...findPerson, number: newNumber})
-          .then(returnedPerson => 
-            setPersons(persons.map(
-              person => person.id != returnedPerson.id ? person : returnedPerson
-            )
-          ))
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id != returnedPerson.id ? person : returnedPerson));
+            showMessage(`${returnedPerson.name} updated`, 'success');
+          })
+          .catch(error => {
+            setPersons(persons.filter(person => person.id != findPerson.id));
+            showMessage(`Information of ${findPerson.name} has already been removed from the server`, 'error');
+          })
       }
     }
   }
@@ -55,10 +70,14 @@ const App = () => {
         .destroy(id)
         .then(response => { 
             if (response.status == 200) {
-              setPersons(persons.filter(person => person.id != id))
+              setPersons(persons.filter(person => person.id != id));
+              showMessage(`${personToDelete.name} deleted`, 'success');
             } 
         })
-        .catch(error => alert(`${personToDelete.name} can't be deleted`))
+        .catch(error => {
+          setPersons(persons.filter(person => person.id != id));
+          showMessage(`${personToDelete.name} has already been removed from the server`, 'error');
+        })
     }
   }
 
@@ -68,6 +87,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message.msg} type={message.type} />
       <Filter 
         name={searchName} 
         searchName={handleSearchName} 
