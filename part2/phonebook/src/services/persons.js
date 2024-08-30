29fb7011c72579601f683db43b0c63ba2a52459e
gcosta815 <gcosta815@gmail.com>
2024-08-29 23:39:31 -0300

    FE build placed in BE root

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://fullstackopen-b5j6.onrender.com/api/persons'
 
 const getAll = () => {
     return axios.get(baseUrl).then(response => response.data)
