ff60b91224740bd0fe43a3abe26d2bd1b1eaab5a
gcosta815 <gcosta815@gmail.com>
2024-09-07 14:19:27 -0300

    uibuild

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -23,7 +23,13 @@ const personSchema = new mongoose.Schema({
     number: {
         type: String,
         required: true,
-        minLength: 8
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{5,}$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
     }
 })
 
