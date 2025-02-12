43de31f99ea5d6ab929ecbb0af07e36d9743ba78
gcosta815 <gcosta815@gmail.com>
2025-02-12 01:16:23 -0300

    Revert "Delete part3/prueba directory"
    This reverts commit 97b20ffa7daf5c1e9dd127a4095b5549ca3946d4.

---
+++ part3/prueba/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://gcosta815:${password}@cluster0.hk3b4.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery',false)
+mongoose.connect(url).then(
+    () => console.log('Connected to MongoDB'),
+)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'nodeJS is really easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+
+// Note.find({important: false}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
